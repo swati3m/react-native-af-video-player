724812134537b243a9ec9b550e93088321ef04a1
swati3m <mailofswati10@gmail.com>
2020-01-20 12:32:45 +0530

    Added hide full screen control

--- components/Controls.js
+++ components/Controls.js
@@ -1,18 +1,12 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 import {
   View,
   Animated,
   StyleSheet,
   TouchableWithoutFeedback as Touchable
-} from 'react-native'
-import {
-  PlayButton,
-  ControlBar,
-  Loading,
-  TopBar,
-  ProgressBar
-} from './'
+} from "react-native";
+import { PlayButton, ControlBar, Loading, TopBar, ProgressBar } from "./";
 
 const styles = StyleSheet.create({
   container: {
@@ -22,39 +16,39 @@ const styles = StyleSheet.create({
   flex: {
     flex: 1
   }
-})
+});
 
 class Controls extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       hideControls: false,
       seconds: 0,
       seeking: false
-    }
-    this.animControls = new Animated.Value(1)
-    this.scale = new Animated.Value(1)
-    this.progressbar = new Animated.Value(2)
+    };
+    this.animControls = new Animated.Value(1);
+    this.scale = new Animated.Value(1);
+    this.progressbar = new Animated.Value(2);
   }
 
   componentDidMount() {
-    this.setTimer()
+    this.setTimer();
   }
 
   componentWillUnmount() {
-    clearInterval(this.timer)
+    clearInterval(this.timer);
   }
 
   onSeek(pos) {
-    this.props.onSeek(pos)
+    this.props.onSeek(pos);
     if (!this.state.seeking) {
-      this.setState({ seeking: true })
+      this.setState({ seeking: true });
     }
   }
 
   onSeekRelease(pos) {
-    this.props.onSeekRelease(pos)
-    this.setState({ seeking: false, seconds: 0 })
+    this.props.onSeekRelease(pos);
+    this.setState({ seeking: false, seconds: 0 });
   }
 
   setTimer() {
@@ -62,47 +56,52 @@ class Controls extends Component {
       switch (true) {
         case this.state.seeking:
           // do nothing
-          break
+          break;
         case this.props.paused:
-          if (this.state.seconds > 0) this.setState({ seconds: 0 })
-          break
+          if (this.state.seconds > 0) this.setState({ seconds: 0 });
+          break;
         case this.state.hideControls:
-          break
-        case this.state.seconds > this.props.controlDuration:
-          this.hideControls()
-          break
+          break;
+        case this.state.seconds > 3:
+          this.hideControls();
+          break;
         default:
-          this.setState({ seconds: this.state.seconds + 1 })
+          this.setState({ seconds: this.state.seconds + 1 });
       }
-    }, 1000)
+    }, 1000);
   }
 
   showControls() {
     this.setState({ hideControls: false }, () => {
-      this.progressbar.setValue(2)
+      this.progressbar.setValue(2);
       Animated.parallel([
         Animated.timing(this.animControls, { toValue: 1, duration: 200 }),
         Animated.timing(this.scale, { toValue: 1, duration: 200 })
-      ]).start()
-    })
+      ]).start();
+    });
   }
 
   hideControls() {
     Animated.parallel([
       Animated.timing(this.animControls, { toValue: 0, duration: 200 }),
       Animated.timing(this.scale, { toValue: 0.25, duration: 200 })
-    ]).start(() => this.setState({ hideControls: true, seconds: 0 }))
+    ]).start(() => this.setState({ hideControls: true, seconds: 0 }));
   }
 
   hiddenControls() {
-    Animated.timing(this.progressbar, { toValue: 0, duration: 200 }).start()
+    Animated.timing(this.progressbar, { toValue: 0, duration: 200 }).start();
     return (
       <Touchable style={styles.container} onPress={() => this.showControls()}>
-        <Animated.View style={[styles.container, { paddingBottom: this.progressbar }]}>
-          <ProgressBar theme={this.props.theme.progress} progress={this.props.progress} />
+        <Animated.View
+          style={[styles.container, { paddingBottom: this.progressbar }]}
+        >
+          <ProgressBar
+            theme={this.props.theme.progress}
+            progress={this.props.progress}
+          />
         </Animated.View>
       </Touchable>
-    )
+    );
   }
 
   loading() {
@@ -110,7 +109,7 @@ class Controls extends Component {
       <View style={styles.container}>
         <Loading theme={this.props.theme.loading} />
       </View>
-    )
+    );
   }
 
   displayedControls() {
@@ -127,15 +126,18 @@ class Controls extends Component {
       currentTime,
       duration,
       theme,
+      // inlineOnly
       inlineOnly,
       hideFullScreenControl
-    } = this.props
+    } = this.props;
 
-    const { center, ...controlBar } = theme
+    const { center, ...controlBar } = theme;
 
     return (
       <Touchable onPress={() => this.hideControls()}>
-        <Animated.View style={[styles.container, { opacity: this.animControls }]}>
+        <Animated.View
+          style={[styles.container, { opacity: this.animControls }]}
+        >
           <TopBar
             title={title}
             logo={logo}
@@ -143,7 +145,9 @@ class Controls extends Component {
             onMorePress={() => onMorePress()}
             theme={{ title: theme.title, more: theme.more }}
           />
-          <Animated.View style={[styles.flex, { transform: [{ scale: this.scale }] }]}>
+          <Animated.View
+            style={[styles.flex, { transform: [{ scale: this.scale }] }]}
+          >
             <PlayButton
               onPress={() => this.props.togglePlay()}
               paused={paused}
@@ -158,8 +162,8 @@ class Controls extends Component {
             muted={muted}
             paused={paused}
             fullscreen={fullscreen}
-            onSeek={pos => this.onSeek(pos)}
-            onSeekRelease={pos => this.onSeekRelease(pos)}
+            onSeek={(pos) => this.onSeek(pos)}
+            onSeekRelease={(pos) => this.onSeekRelease(pos)}
             progress={progress}
             currentTime={currentTime}
             duration={duration}
@@ -169,15 +173,15 @@ class Controls extends Component {
           />
         </Animated.View>
       </Touchable>
-    )
+    );
   }
 
   render() {
-    if (this.props.loading) return this.loading()
+    if (this.props.loading) return this.loading();
     if (this.state.hideControls) {
-      return this.hiddenControls()
+      return this.hiddenControls();
     }
-    return this.displayedControls()
+    return this.displayedControls();
   }
 }
 
@@ -201,6 +205,6 @@ Controls.propTypes = {
   title: PropTypes.string.isRequired,
   logo: PropTypes.string.isRequired,
   theme: PropTypes.object.isRequired
-}
+};
 
-export { Controls }
+export { Controls };
